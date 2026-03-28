71a53d9fc1b179e055c732d47b6c36d76132d90b
Amit Eisinger <amiteisinger@campus.technion.ac.il>
2026-03-28 15:47:33 +0300

    Insert learnt lemma to highest frame
    We learn lemmas as part of the CTG process.
    Rather than inserting the learnt lemma to the exact frame we insert it
    to the highest frame possible.

--- pdrer_crate/src/engines/pdr/frames/generalize_with_ctg.rs
+++ pdrer_crate/src/engines/pdr/frames/generalize_with_ctg.rs
@@ -101,7 +101,7 @@ impl<T: PropertyDirectedReachabilitySolver, D: DecisionDiagramManager> Frames<T,
                             .weights
                             .borrow_mut()
                             .update_weights_on_add(de.clause().iter());
-                        self.insert_clause_to_exact_frame(de, j, false);
+                        self.insert_clause_to_highest_frame_possible(de.unpack_clause(), j);
                     } else {
                         ctgs = 0;
                         *clause = clause
